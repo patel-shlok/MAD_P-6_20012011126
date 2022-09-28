98f4111d23920433ac84e7a5e6e0cbaa5ca1d164
patel-shlok <patelshlok11@gmail.com>
2022-09-28 10:07:37 +0530

    commit

--- app/src/main/java/com/example/shlok_pr6/MainActivity.kt
+++ app/src/main/java/com/example/shlok_pr6/MainActivity.kt
@@ -4,25 +4,15 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import android.widget.VideoView
-import androidx.core.view.WindowCompat
-import com.example.shlok_pr6.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        initVideoPlayer()
-    }
-    private fun initVideoPlayer(){
-        var MediaController= mediaController
+        setContentView(R.layout.activity_main)
+        val mediaController=MediaController(this)
         val uri:Uri=Uri.parse("android.resource://"+packageName+"/"+R.raw.thestoryoflight)
         val myvideoview=findViewById<VideoView>(R.id.video)
         myvideoview.setMediaController(mediaController)
@@ -34,6 +24,5 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener{
             Intent(this,YoutubeActivity::class.java).apply{startActivity(this)}
         }
-
     }
 }
